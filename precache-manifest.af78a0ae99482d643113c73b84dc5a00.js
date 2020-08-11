681bc7fe52bbbfc8eaf16aec7ad1171fea6bef7c
eddyangang <eddyyang@berkeley.edu>
2020-08-11 01:01:32 -0700

    Updates

---
+++ precache-manifest.af78a0ae99482d643113c73b84dc5a00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd372fa89c90582300ed1e91b4561053",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "8652ac33498d9a506c74",
+    "url": "/clicky-game/static/css/main.62290590.chunk.css"
+  },
+  {
+    "revision": "77ec5a0aa6834a00ede5",
+    "url": "/clicky-game/static/js/2.69b5d891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.69b5d891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8652ac33498d9a506c74",
+    "url": "/clicky-game/static/js/main.7d7207a5.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  }
+]);
